870a623c484b745fa5b083067ea9a997a21a6d8e
Stanisław Jarocki <s.jarocki354@gmail.com>
2024-04-20 09:19:18 +0200

    wip

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,25 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import { MongoClient } from "mongodb";
 
 export default component$(() => {
+
+  async function main() {
+    const uri = `mongodb+srv://stanlee77:${process.env.DB_PASSWORD}@cluster0.badzthl.mongodb.net/`;
+    const client = new MongoClient(uri);
+
+    try {
+      await client.connect();
+      const database = client.db('test');
+      console.log('database', database)
+    } catch (e) {
+      console.error(e);
+    } finally {
+      await client.close();
+    }
+  }
+
+  main().catch(console.error);
   return (
     <>
       <h1>Hi 👋</h1>
@@ -15,11 +33,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Worklogs assistant",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "todo",
     },
   ],
 };
